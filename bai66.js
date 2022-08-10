3bfa62261b61ea92c47ac086670b07201f865d45
Mike <mike@dinoland.io>
2022-08-10 21:59:55 +0700

    get element method

---
+++ bai66.js
@@ -0,0 +1,21 @@
+// get element by ID
+// var headingNode = document.getElementById('headingnode');
+// console.log({
+//   element: headingNode,
+// });
+
+
+//get element by class
+// var headingNodes = document.getElementsByClassName('headingnode');
+// console.log(headingNodes);
+
+//get element by tag name
+// var headingNodes = document.getElementsByTagName('h1');
+// console.log(headingNodes);
+
+//get element by css selector
+// var headingNodes = document.querySelector('.headingnode .hhh:first-child');
+// console.log(headingNodes);
+
+//get element by html collection
+console.log(document.forms[1]);
