34be470f4440edb84f081b9b6a716d6c13b61f81
Mike <mike@dinoland.io>
2022-08-12 22:31:53 +0700

    hocbai 74

---
+++ bai74.js
@@ -0,0 +1,11 @@
+var boxElement =
+    document.querySelector('.box');
+// boxElement.style.width = '100px';
+// boxElement.style.height = '200px';
+// boxElement.style.backgroundColor = 'red';
+Object.assign(boxElement.style, {
+  width: '200px',
+  height: '100px',
+  backgroundColor: 'green',
+});
+console.log(boxElement.style.backgroundColor);
